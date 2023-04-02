f450a4ac3ccd53830b837c13d344664356c9af62
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2023-04-02 19:15:40 +0200

    data and translations added to the GUI

--- barnatrees/barnatrees.sh
+++ barnatrees/barnatrees.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-sqlite3 -batch barnatrees.db 'select description from summary where name like "treedbcreator%"' > barnatrees.txt
 
+if [ ! -r barnatrees.txt ]; then
+    sqlite3 -batch barnatrees.db 'select description from summary where name like "treedbcreator%"' > barnatrees.txt
+fi
+
+if [ ! -e barnatrees.db.7z ]; then
+    7z a barnatrees.db.7z barnatrees.db
+fi
